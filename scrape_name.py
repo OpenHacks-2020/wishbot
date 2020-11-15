9d590225ca73cffe1f02d5bf8012247746f39dea
anoushkas02 <64431315+anoushkas02@users.noreply.github.com>
2020-11-15 10:58:01 -0800

    Create scrape_name.py

---
+++ scrape_name.py
@@ -0,0 +1,14 @@
+# #-*- coding: utf-8 -*-
+from bs4 import BeautifulSoup
+import requests
+
+def getName(url):
+'''Takes a product URL as an input and isolates and prints the product's name/title'''
+	content = requests.get(url).content
+	soup = BeautifulSoup(content, features='lxml')
+	product_names = str(soup.findAll('title')[0])
+	list_pn = product_names.split()
+	list_pn.remove('|')
+	list_pn.remove('Etsy</title>')
+	list_pn[0] = list_pn[0][7:]
+	print(' '.join(list_pn))
